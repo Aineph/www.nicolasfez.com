a7e2e6ec6783555a91340d26c288538ffe846d5c
Nicolas Fez <nicolasfez@MacBook-Pro-de-Nicolas.local>
2025-02-25 14:15:49 +0100

    refactor: Applied linting on project files

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -11,6 +11,6 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
     return {
         locale,
-        messages: await (import ((`../messages/${locale}.json`)))
+        messages: await (import ((`../messages/${locale}.json`))),
     }
 })
